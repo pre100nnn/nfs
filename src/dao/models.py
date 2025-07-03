cf8ea9c2c859573b0bb423366fdfcea6e0db088c
MaxS <pre100nnn@gmail.com>
2025-07-03 15:15:29 +0300

    add model users

--- src/dao/models.py
+++ src/dao/models.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine, Column, Integer, String, DateTime, Boolean
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 
 from datetime import datetime
 
@@ -11,14 +11,18 @@ class User(Base):
     """Модель пользователя"""
     __tablename__ = 'nfs_users'
 
-   telegram_id = Column(Integer, primary_key=True)
-   username = Column(String, nullable=True)
-   first_name = Column(String,nullable=False)
-   last_name = Column(String,nullable=True)
-   birthday = Column(DateTime, nullable=False)
-   is_admin = Column(Boolean,default=False)
-   is_active = Column(Boolean,default=True)
-   registered_at = Column(DateTime,default=datetime.utcnow)
+    telegram_id = Column(Integer, primary_key=True)
+    username = Column(String, nullable=True)
+    first_name = Column(String,nullable=False)
+    last_name = Column(String,nullable=True)
+    birthday = Column(DateTime, nullable=False)
+    is_admin = Column(Boolean,default=False)
+    is_active = Column(Boolean,default=True)
+    registered_at = Column(DateTime,default=datetime.utcnow)
+
 
     def __repr__(self):
-        return f"<User(id={self.telegram_id}, username='{self.username}')>
+        return f"<User(id={self.telegram_id}, username='{self.username}')>"
+
+engine=create_engine('sqlite:///nfs_users.db',echo=True)
+Base.metadata.create_all(engine)
