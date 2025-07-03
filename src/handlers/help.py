6d20ece869a25b746517a3ef4ddbe0b56dc86a2b
MaxS <pre100nnn@gmail.com>
2025-07-03 15:00:08 +0300

    add model users

--- src/handlers/help.py
+++ src/handlers/help.py
@@ -1,6 +1,4 @@
-from src.core import bot
-
 def register_help_handlers(bot):
     @bot.message_handler(commands=['help'])
     async def help(message):
-        await bot.reply_to(message, "Помоги мне)")
+        await bot.reply_to(message, "Помощь: /start, /help")
--- src/handlers/start.py
+++ src/handlers/start.py
@@ -1,5 +1,3 @@
-from src.core import bot
-
 def register_start_handlers(bot):
     @bot.message_handler(commands=['start'])
     async def start(message):
